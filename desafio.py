a2ae915d76a920f54214e3ef960b550a8c24187b
Michael <michael.mjpr@gmail.com>
2026-02-24 18:24:07 -0300

    importando módulo de erro

--- desafio.py
+++ desafio.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.exc import SQLAlchemyError
 
 
 Base = declarative_base()
@@ -25,6 +26,12 @@ class Produto(Base):
     fornecedor = relationship("Fornecedor")
     
 
+
+engine =  create_engine('sqlite:///desafio.db', echo=True)
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 
